d6d07a87f792a493d01087be7dee8d10816da56c
Hakan Baştürk <forxynet@gmail.com>
2024-01-26 16:14:25 +0300

    modified some files

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,8 +8,6 @@ import { PaymentCreatedListener } from './events/listeners/payment-created-liste
 
 const start = async () => {
 
-  console.log('Starting Order Service');
-
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
@@ -52,7 +50,7 @@ const start = async () => {
   }
 
   app.listen(3000, () => {
-    console.log('Listening on port 3000!');
+    console.log('Listening on port 3000');
   });
 };
 
